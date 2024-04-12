44b172d3dd6208df5f44a767cd37aff5139f079d
Muskan <muskanoad55@gmail.com>
2024-04-12 13:56:18 -0500

    Connected with the DB

--- server/profilemanage.inc.php
+++ server/profilemanage.inc.php
@@ -20,7 +20,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           SELECT :Name, :address_1, :address_2, :city, :state, :zip, users.user_id
           FROM users
           WHERE users.username = :username";
+        // storing the user entered data into the database
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(":Name",$full_name);
+        $stmt->bindParam(":address_1",$address_1);
+        $stmt->bindParam(":address_2",$address_2);
+        $stmt->bindParam(":city",$city);
+        $stmt->bindParam(":state",$state);
+        $stmt->bindParam(":zip",$zip);
+        $stmt->bindParam(":username", $user);
+        $stmt->execute();
 
+        $pdo = null;
+        $stmt = null;
         
         header("Location: ../Assignment4/loginhome.php");
         exit();
