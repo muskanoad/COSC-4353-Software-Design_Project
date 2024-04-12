b1655ca93055acd74d893a4b53661b9eb438666e
Muskan <muskanoad55@gmail.com>
2024-04-12 14:23:18 -0500

    Updated

--- server/userlogin.inc.php
+++ server/userlogin.inc.php
@@ -18,8 +18,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $result = $stmt->fetch(PDO::FETCH_ASSOC);
             return $result; 
         }
-        
-        
+        // Retrieve user information from the database
+        $user = get_user($pdo, $username);
+
+        if ($user) {
+            // Verify user
+            $_SESSION['user'] = $user['username'];
+            header("Location: ../Assignment4/loginhome.php");
+         
+        } else {
+            // User not found, redirect back to login page with error
+            $_SESSION['error'] = 'Invalid username or password.';
+            header("Location: ../Assignment4/login.php?error=2");
+            exit();
+        }
     } catch (PDOException $e) {
         die("Query failed: ". $e->getMessage());
     }
